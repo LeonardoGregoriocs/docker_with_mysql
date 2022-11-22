ebddccaf77ab196260909f22c9cc5e17936815a5
Leonardogregoriocs <leonardogregoriocs@gmail.com>
2022-11-22 18:22:49 -0300

    Atualiza arquivos: docker-compose, Dockerfile e index.js

--- node/index.js
+++ node/index.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const people1 = `INSERT INTO people(name) values('Leonardo')`
+const people2 = `INSERT INTO people(name) values('Edivane')`
+const people3 = `INSERT INTO people(name) values('Lucas')`
+
+connection.query(people1)
+connection.query(people2)
+connection.query(people3)
+connection.end()
 
 app.get('/', (req, res) => {
     res.send('<h1>Full Cycle</h1>')
